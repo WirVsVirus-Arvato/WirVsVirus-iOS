9a282e4ef025c904dd71059539f04cbb7d402e54
Böhm-de Silva, Christoph <christoph.boehm-desilva@bertelsmann.de>
2020-03-19 19:10:13 +0100

    init commit

---
+++ WirVsVirus/WirVsVirus/WVVHomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WirVsVirus
+//
+//  Created by Böhm-de Silva, Christoph on 19.03.20.
+//  Copyright © 2020 Böhm-de Silva, Christoph. All rights reserved.
+//
+
+import UIKit
+
+class WVVHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WirVsVirus/WirVsVirusTests/WirVsVirusTests.swift
@@ -0,0 +1,34 @@
+//
+//  WirVsVirusTests.swift
+//  WirVsVirusTests
+//
+//  Created by Böhm-de Silva, Christoph on 19.03.20.
+//  Copyright © 2020 Böhm-de Silva, Christoph. All rights reserved.
+//
+
+import XCTest
+@testable import WirVsVirus
+
+class WirVsVirusTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
